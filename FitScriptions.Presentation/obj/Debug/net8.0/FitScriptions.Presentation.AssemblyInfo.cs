888d0870dff9984dbd83729567f379b7258827e3
silwesterabram <silwesterabram@gmail.com>
2024-04-19 10:27:29 +0300

    init

--- FitScriptions.Presentation/obj/Debug/net8.0/FitScriptions.Presentation.AssemblyInfo.cs
+++ FitScriptions.Presentation/obj/Debug/net8.0/FitScriptions.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitScriptions.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2799560e1ada3226a9743a079f1098c2fe505d6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitScriptions.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitScriptions.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2799560e1ada3226a9743a079f1098c2fe505d6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
